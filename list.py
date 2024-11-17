a344c9c56b92fff396b0992b8ca1e678c69f18b2
Kevin Camilo Gómez González <sickedgoke@gmail.com>
2024-11-16 20:07:53 -0500

    Create list.py

---
+++ list.py
@@ -0,0 +1,20 @@
+import os
+
+def listar_estructura(carpeta_base, excluir=[".venv", "__pycache__", ".git"], nivel=0):
+    for elemento in os.listdir(carpeta_base):
+        ruta = os.path.join(carpeta_base, elemento)
+        
+        # Comprobar si el elemento es una carpeta o archivo excluido
+        if os.path.basename(ruta) in excluir:
+            continue
+
+        # Imprimir la estructura
+        print(" " * nivel * 2 + f"|-- {elemento}")
+
+        # Si es un directorio, recorrerlo recursivamente
+        if os.path.isdir(ruta):
+            listar_estructura(ruta, excluir, nivel + 1)
+
+# Cambia "ruta_del_proyecto" por la ruta base de tu proyecto
+ruta_del_proyecto = "K:/Development/CURSOS/UNIANDES/Modulo 2/Proyecto_2_kc.gomez"
+listar_estructura(ruta_del_proyecto)
